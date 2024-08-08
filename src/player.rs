59727457c3e0c0ea67b0eb5ca30d526db63e2234
Vitalii Shevchuk <vitshev120@gmail.com>
2024-08-08 22:24:32 +0300

    made stars spawn

--- src/player.rs
+++ src/player.rs
@@ -1,9 +1,9 @@
-use bevy::app::{ Plugin, Startup, Update };
+use bevy::app::{Plugin, Startup, Update};
 use bevy::asset::AssetServer;
 use bevy::core_pipeline::core_2d::Camera2dBundle;
 use bevy::ecs::component::Component;
 use bevy::ecs::query::With;
-use bevy::ecs::system::{ Commands, Query, Res };
+use bevy::ecs::system::{Commands, Query, Res};
 use bevy::input::keyboard::KeyCode;
 use bevy::input::ButtonInput;
 use bevy::math::Vec3;
@@ -11,9 +11,9 @@ use bevy::math::Vec3;
 use bevy::sprite::SpriteBundle;
 use bevy::time::Time;
 use bevy::transform::components::Transform;
-use bevy::window::{ PrimaryWindow, Window };
+use bevy::window::{PrimaryWindow, Window};
 
-use crate::{ HALF_PLAYER_SIZE, PLAYER_SPEED };
+use crate::{HALF_PLAYER_SIZE, PLAYER_SPEED};
 
 #[derive(Component)]
 pub struct Player {}
@@ -32,7 +32,7 @@ impl Plugin for PlayerPlugin {
 pub fn spawn_player(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
 
@@ -58,7 +58,7 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
 pub fn player_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut player_query: Query<&mut Transform, With<Player>>,
-    time: Res<Time>
+    time: Res<Time>,
 ) {
     if let Ok(mut transform) = player_query.get_single_mut() {
         let mut direction = Vec3::ZERO;
@@ -85,7 +85,7 @@ pub fn player_movement(
 
 pub fn confine_player_movement(
     mut player_query: Query<&mut Transform, With<Player>>,
-    window_query: Query<&Window, With<PrimaryWindow>>
+    window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     if let Ok(mut transform) = player_query.get_single_mut() {
         let window = window_query.get_single().unwrap();
--- src/star.rs
+++ src/star.rs
@@ -1,21 +1,29 @@
 use bevy::{
-    app::{ Plugin, Startup },
+    app::{Plugin, Startup, Update},
     asset::AssetServer,
-    ecs::{ component::Component, query::With, system::{ Commands, Query, Res, Resource } },
+    ecs::{
+        component::Component,
+        query::With,
+        system::{Commands, Query, Res, Resource},
+    },
+    prelude::ResMut,
     sprite::SpriteBundle,
-    time::{ Timer, TimerMode },
+    time::{Time, Timer, TimerMode},
     transform::components::Transform,
-    window::{ PrimaryWindow, Window },
+    window::{self, PrimaryWindow, Window},
 };
 use rand::random;
 
-use crate::{ NUMBER_OF_STARS, STAR_SPAWN_TIME };
+use crate::{NUMBER_OF_STARS, STAR_SPAWN_TIME};
 
 pub struct StarPlugin;
 
 impl Plugin for StarPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.init_resource::<StarSpawnTimer>().add_systems(Startup, spawn_stars);
+        app.init_resource::<StarSpawnTimer>()
+            .add_systems(Startup, spawn_stars)
+            .add_systems(Update, tick_star_spawn_timer)
+            .add_systems(Update, spawn_stars_over_time);
     }
 }
 
@@ -38,11 +46,37 @@ pub struct Star {}
 pub fn spawn_stars(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
 
     for _ in 0..NUMBER_OF_STARS {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/star.png"),
+                ..Default::default()
+            },
+            Star {},
+        ));
+    }
+}
+
+pub fn tick_star_spawn_timer(mut star_spawn_timer: ResMut<StarSpawnTimer>, time: Res<Time>) {
+    star_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_stars_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    star_spawn_timer: ResMut<StarSpawnTimer>,
+) {
+    if star_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
         let random_x = random::<f32>() * window.width();
         let random_y = random::<f32>() * window.height();
 
